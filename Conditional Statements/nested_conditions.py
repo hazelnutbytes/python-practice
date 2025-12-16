52c3cbd59d84b4c0437c3ace7de720c8544dcc44
Hazel Lakshmana <hazellakshmana684@gmail.com>
2025-12-17 04:43:32 +0530

    Create nested_conditions.py

---
+++ Conditional Statements/nested_conditions.py
@@ -0,0 +1,9 @@
+age = 20
+has_id = False
+if age >= 18:
+    if has_id:
+        print("You are allowed to enter.")
+    else:
+        print("You need to show your ID.")
+else:
+    print("You are not allowed to enter.")
