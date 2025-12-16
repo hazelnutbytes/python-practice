84a974cd545ea839bb6db3a4a428f4d4487a52b3
Hazel Lakshmana <hazellakshmana684@gmail.com>
2025-12-17 04:46:53 +0530

    Add try-except block for exception handling

---
+++ Exception Handling/try_except.py
@@ -0,0 +1,11 @@
+try:
+    num = int(input("Enter number: "))
+    print(10 / num)
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+except ValueError:
+    print("Invalid input.")
+else:
+    print("Operation successful.")
+finally:
+    print("Program ended.")
