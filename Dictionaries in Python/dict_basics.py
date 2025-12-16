6a81c3c0292d813510159026a042d2fd23f05e1a
Hazel Lakshmana <hazellakshmana684@gmail.com>
2025-12-17 04:42:15 +0530

    Add basic dictionary example in dict_basics.py

---
+++ Dictionaries in Python/dict_basics.py
@@ -0,0 +1,11 @@
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "colours": ["red", "white", "blue"]
+}
+print(thisdict)
+print(thisdict["brand"])
+print(thisdict["colours"][0])
+thisdict["year"] = 2020
+print(thisdict)
