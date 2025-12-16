7ebd1cf5210742bb54593745d2e6a35587321fbc
Hazel Lakshmana <hazellakshmana684@gmail.com>
2025-12-17 04:51:13 +0530

    Add array operations for statistical calculations

---
+++ Numpy/array_operations.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.array([10,20,30,40,50])
+print('Mean:', np.mean(arr))
+print('Median:', np.median(arr))
+print('Std Dev:', np.std(arr))
+print('Variance:', np.var(arr))
+print('Sum:', np.sum(arr))
+print('Min:', np.min(arr))
+print('Max:', np.max(arr))
